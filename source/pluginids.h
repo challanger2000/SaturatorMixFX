ad943277c866224340cde4b3372c523c3ef298f8
challanger2000 <challanger2000@gmx.de>
2026-09-10 02:48:05 +0200

    Add On Off parameter ID

--- source/pluginids.h
+++ source/pluginids.h
@@ -8,10 +8,11 @@ namespace SaturatorMixFX {
 static const Steinberg::FUID kProcessorUID (0x74D9F51A, 0x2E754BC8, 0xA9439C72, 0x1DB0A1F4);
 static const Steinberg::FUID kControllerUID (0x1D27F0E2, 0xA98442CE, 0xB8B0B6D1, 0x90E83753);
 
-constexpr Steinberg::Vst::ParamID kParamDrive     = 100;
-constexpr Steinberg::Vst::ParamID kParamCharacter = 101;
-constexpr Steinberg::Vst::ParamID kParamMix       = 102;
-constexpr Steinberg::Vst::ParamID kParamOutput    = 103;
+constexpr Steinberg::Vst::ParamID kParamOnOff      = 99;
+constexpr Steinberg::Vst::ParamID kParamDrive      = 100;
+constexpr Steinberg::Vst::ParamID kParamCharacter  = 101;
+constexpr Steinberg::Vst::ParamID kParamMix        = 102;
+constexpr Steinberg::Vst::ParamID kParamOutput     = 103;
 
 enum Character : int32_t {
     kTriode = 0,
