fb97e06b300059366c5a166b84daaf623970ffe1
challanger2000 <challanger2000@gmx.de>
2026-09-10 11:34:24 +0200

    Correct SMX-3 processor defaults

--- source/processor.h
+++ source/processor.h
@@ -15,24 +15,22 @@ public:
 
     Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown* context) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API setBusArrangements(
-        Steinberg::Vst::SpeakerArrangement* inputs,
-        Steinberg::int32 numIns,
-        Steinberg::Vst::SpeakerArrangement* outputs,
-        Steinberg::int32 numOuts) SMTG_OVERRIDE;
+        Steinberg::Vst::SpeakerArrangement* inputs, Steinberg::int32 numIns,
+        Steinberg::Vst::SpeakerArrangement* outputs, Steinberg::int32 numOuts) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API canProcessSampleSize(Steinberg::int32 symbolicSampleSize) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API process(Steinberg::Vst::ProcessData& data) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API setState(Steinberg::IBStream* state) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API getState(Steinberg::IBStream* state) SMTG_OVERRIDE;
 
 private:
-    double onOff_ = 1.0;
-    double drive_ = 0.30;
-    double character_ = 0.0;
-    double mix_ = 1.0;
-    double output_ = 0.50;
-
     void readParameterChanges(Steinberg::Vst::IParameterChanges* changes);
     float saturate(float x) const;
+
+    double onOff_ = 0.0;      // VST3 bypass: 0 = active, 1 = bypass
+    double drive_ = 0.30;
+    double character_ = 0.0;  // Triode default
+    double mix_ = 1.0;
+    double output_ = 0.75;    // 0 dB in current mapping
 };
 
 } // namespace SaturatorMixFX
