e3fee21225ccb4c98b52e415643526b9c24489f3
challanger2000 <challanger2000@gmx.de>
2026-09-10 11:33:53 +0200

    Wire SMX-3 custom editor into controller

--- source/controller.h
+++ source/controller.h
@@ -15,6 +15,7 @@ public:
 
     Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown* context) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API setComponentState(Steinberg::IBStream* state) SMTG_OVERRIDE;
+    Steinberg::IPlugView* PLUGIN_API createView(Steinberg::FIDString name) SMTG_OVERRIDE;
 };
 
 } // namespace SaturatorMixFX
