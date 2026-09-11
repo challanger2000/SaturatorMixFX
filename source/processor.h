5da45ab649a7806cdbbd9e67c0386560b7e7e86c
challanger2000 <challanger2000@gmx.de>
2026-09-11 02:54:15 +0200

    Rebuild SMX saturation DSP architecture

--- source/processor.h
+++ source/processor.h
@@ -2,6 +2,8 @@
 
 #include "public.sdk/source/vst/vstaudioeffect.h"
 
+#include <array>
+
 namespace SaturatorMixFX {
 
 class Processor final : public Steinberg::Vst::AudioEffect {
@@ -18,20 +20,48 @@ public:
         Steinberg::Vst::SpeakerArrangement* inputs, Steinberg::int32 numIns,
         Steinberg::Vst::SpeakerArrangement* outputs, Steinberg::int32 numOuts) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API canProcessSampleSize(Steinberg::int32 symbolicSampleSize) SMTG_OVERRIDE;
+    Steinberg::tresult PLUGIN_API setupProcessing(Steinberg::Vst::ProcessSetup& setup) SMTG_OVERRIDE;
+    Steinberg::tresult PLUGIN_API setActive(Steinberg::TBool state) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API process(Steinberg::Vst::ProcessData& data) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API setState(Steinberg::IBStream* state) SMTG_OVERRIDE;
     Steinberg::tresult PLUGIN_API getState(Steinberg::IBStream* state) SMTG_OVERRIDE;
 
 private:
+    static constexpr int kMaxChannels = 2;
+    static constexpr int kOversample = 4;
+
+    struct ChannelState {
+        double previousInput = 0.0;
+        double ironMemory = 0.0;
+        double dcX1 = 0.0;
+        double dcY1 = 0.0;
+    };
+
     void readParameterChanges(Steinberg::Vst::IParameterChanges* changes);
-    float saturate(float x) const;
-    float magic(float x, float saturated) const;
+    void resetDsp();
+    void updateSmoothers();
+
+    double shapeTriode(double x) const;
+    double shapePentode(double x) const;
+    double shapeIron(double x, ChannelState& state);
+    double processNonlinear(double x, int mode, ChannelState& state);
+    double dcBlock(double x, ChannelState& state);
 
     double onOff_ = 0.0;
     double drive_ = 0.30;
     double character_ = 0.0;
     double mix_ = 1.0;
     double output_ = 0.75;
+
+    double sampleRate_ = 44100.0;
+    double smoothDrive_ = 0.30;
+    double smoothMix_ = 1.0;
+    double smoothOutput_ = 0.75;
+    double smoothCoeff_ = 0.0;
+    double ironMemoryCoeff_ = 0.0;
+    double dcCoeff_ = 0.995;
+
+    std::array<ChannelState, kMaxChannels> channelState_{};
 };
 
 } // namespace SaturatorMixFX
